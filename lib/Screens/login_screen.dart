9a595f3de3d3fcaae44f403d30c6db9d29d17183
TangClue <124027059+tangclue@users.noreply.github.com>
2023-02-15 18:37:55 +0900

    app start with wait

--- lib/Screens/login_screen.dart
+++ lib/Screens/login_screen.dart
@@ -20,75 +20,76 @@ class LoginScreen extends StatelessWidget {
       SystemChrome.setPreferredOrientations([DeviceOrientation.portraitUp]);
     });
 
-    return Scaffold(
-      body: SafeArea(
-        child: SingleChildScrollView(
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.center,
-            children: [
-              Padding(
-                padding:
-                    const EdgeInsets.only(top: Sizes.size80 + Sizes.size80),
+    return FutureBuilder(
+      future: Future.delayed(const Duration(seconds: 2)),
+      builder: (context, snapshot) {
+        if (snapshot.connectionState == ConnectionState.done) {
+          return Scaffold(
+            body: SafeArea(
+              child: SingleChildScrollView(
                 child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.center,
                   children: [
-                    const Text(
-                      "수학이 재미있다. Tang Clue",
-                      style: TextStyle(
-                        fontSize: Sizes.size24,
-                        fontWeight: FontWeight.w700,
+                    Padding(
+                      padding: const EdgeInsets.only(
+                          top: Sizes.size80 + Sizes.size80),
+                      child: Column(
+                        children: [
+                          const Text(
+                            "수학이 재미있다. Tang Clue",
+                            style: TextStyle(
+                              fontSize: Sizes.size24,
+                              fontWeight: FontWeight.w700,
+                            ),
+                            textAlign: TextAlign.center,
+                          ),
+                          Gaps.v20,
+                          Text(
+                            "힌트와 함께하는 재밌는 수학",
+                            style: TextStyle(
+                              fontSize: Sizes.size24,
+                              color: Colors.grey.shade600,
+                            ),
+                          ),
+                        ],
                       ),
-                      textAlign: TextAlign.center,
                     ),
-                    Gaps.v20,
-                    Text(
-                      "힌트와 함께하는 재밌는 수학",
-                      style: TextStyle(
-                        fontSize: Sizes.size24,
-                        color: Colors.grey.shade600,
+                    Padding(
+                      padding:
+                          const EdgeInsets.symmetric(horizontal: Sizes.size24),
+                      child: FractionallySizedBox(
+                        // heightFactor: 0.3,
+                        child: Image.asset("assets/image1.png"),
                       ),
                     ),
+                    Align(
+                      child: Padding(
+                        padding: const EdgeInsets.only(bottom: Sizes.size80),
+                        child: CupertinoButton(
+                          onPressed: () {
+                            _onTabNext(context);
+                          },
+                          color: Theme.of(context).primaryColor,
+                          child: const Text(
+                            "계속하기",
+                            style: TextStyle(
+                                fontSize: Sizes.size36,
+                                fontWeight: FontWeight.w600),
+                          ),
+                        ),
+                      ),
+                    )
                   ],
                 ),
               ),
-              Padding(
-                padding: const EdgeInsets.symmetric(horizontal: Sizes.size24),
-                child: FractionallySizedBox(
-                  // heightFactor: 0.3,
-                  child: Image.asset("assets/image1.png"),
-                ),
-              ),
-              Align(
-                child: Padding(
-                  padding: const EdgeInsets.only(bottom: Sizes.size80),
-                  child: CupertinoButton(
-                    onPressed: () {
-                      _onTabNext(context);
-                    },
-                    color: Theme.of(context).primaryColor,
-                    child: const Text(
-                      "계속하기",
-                      style: TextStyle(
-                          fontSize: Sizes.size36, fontWeight: FontWeight.w600),
-                    ),
-                  ),
-                ),
-              )
-            ],
-          ),
-        ),
-      ),
-      // bottomNavigationBar: BottomAppBar(
-      //   height: Sizes.size80 + Sizes.size80 + Sizes.size80,
-      //   child: CupertinoButton(
-      //     onPressed: () {},
-      //     color: Theme.of(context).primaryColor,
-      //     child: const Text(
-      //       "계속하기",
-      //       style:
-      //           TextStyle(fontSize: Sizes.size36, fontWeight: FontWeight.w600),
-      //     ),
-      //   ),
-      // ),
+            ),
+          );
+        } else {
+          return const Scaffold(
+            body: Center(child: CircularProgressIndicator()),
+          );
+        }
+      },
     );
   }
 }
